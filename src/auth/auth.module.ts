d4c6860f8b80e651087d518bf42d7d6c4061fb46
chango <kimtothechang@gmail.com>
2022-10-09 00:31:00 +0900

    fix: add UserRepository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import * as config from 'config';
+import { TypeOrmExModule } from 'src/libs/typeorm-ex.module';
+import { UserRepository } from './auth.repository';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'HASecret2022',
+      secret: process.env.JWT_SECRET || config.get('jwt.secret'),
       signOptions: {
         expiresIn: 60 * 60,
       },
     }),
+    TypeOrmExModule.forCustomRepository([UserRepository]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
