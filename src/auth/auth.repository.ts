3c0781301c93811d6b0825e92815bf2c1161621a
chango <kimtothechang@gmail.com>
2022-10-09 00:43:42 +0900

    fix: nickname

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -15,7 +15,7 @@ export class UserRepository extends Repository<User> {
 
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
-    const user = this.create({ username, password: hashedPassword });
+    const user = this.create({ username, password: hashedPassword, nickname });
 
     try {
       await this.save(user);
