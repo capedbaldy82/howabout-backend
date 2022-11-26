56d7e05846c5b4b6c642ea4264e8241b0bad0cd2
chango <kimtothechang@gmail.com>
2022-11-27 02:09:22 +0900

    test: change type

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -27,8 +27,8 @@ export class ProductService {
     return sorted_porducts;
   }
 
-  async getProductById(id: number): Promise<Product> {
-    const found = await this.productRepository.findOne({ where: { id } });
+  async getProductById(id: string): Promise<Product> {
+    const found = await this.productRepository.findOne({ where: { id: +id } });
 
     this.logger.verbose(`getProductById is called with ${id}`);
 
