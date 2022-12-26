75950990e2b9de1894f1dc2e380b42b4b8475d49
chango <kimtothechang@gmail.com>
2022-12-26 17:41:42 +0900

    feat: getAllUserInfo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -48,12 +48,14 @@ export class AuthController {
   @Get('/userinfo')
   @UseGuards(AuthGuard())
   getUserInfo(@GetUser() user: User) {
-    const userinfo = {
-      ...user,
-      password: '',
-    };
+    return this.authService.getUserInfo(user);
+  }
 
-    return userinfo;
+  // 모든 유저 정보 확인
+  @Get('/alluserinfo')
+  @UseGuards(AuthGuard())
+  getAllUserInfo(@GetUser() user: User) {
+    return this.authService.getAllUserInfo(user);
   }
 
   // 장바구니 추가
